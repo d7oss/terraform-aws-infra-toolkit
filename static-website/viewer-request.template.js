0bf4606e75691813c53aa782ca49a99590426f7a
Evandro Myller <emyller@d7.dev>
2024-04-26 13:19:12 -0300

    static-website: Fix asset matcher

--- static-website/viewer-request.template.js
+++ static-website/viewer-request.template.js
@@ -1,6 +1,6 @@
 function handler(event) {
   var request = event.request;
-  var is_asset = (/\.[^\.]{2,}$/).test(request.uri);
+  var is_asset = (/\.[a-zA-Z0-9]{1,8}$/).test(request.uri);
   request.uri = is_asset ? request.uri : '/${root_object}';
   return request;
 }
